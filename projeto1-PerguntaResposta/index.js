5562405603ff3aeffbf9184e5ca87069bc63b118
Geisy Poncio <geisygabri@gmail.com>
2023-04-03 08:37:55 -0300

    feat: add answer database and page to answer

--- projeto1-PerguntaResposta/index.js
+++ projeto1-PerguntaResposta/index.js
@@ -3,6 +3,7 @@ const app = express();
 const bodyParser = require("body-parser");
 const connection = require("./database/database");
 const Pergunta = require("./database/Pergunta");
+const Resposta = require("./database/Resposta");
 
 connection
     .authenticate()
@@ -41,6 +42,33 @@ app.post("/salvarpergunta", (req, res) =>{
         descricao: descricao
     }).then(() => {
         res.redirect("/");  //depois de salvo redireciona para a principal
+    });
+});
+
+app.get("/pergunta/:id", (req, res) =>{
+    let id = req.params.id;
+    Pergunta.findOne({
+        where: {id: id}
+    }).then(pergunta =>{
+        if(pergunta != undefined) {
+            res.render("pergunta", {
+                pergunta: pergunta
+            });
+        }
+        else
+            res.redirect("/");
+    });
+});
+
+app.post("/responder", (req, res) =>{
+    let corpo = req.body.corpo;
+    let perguntaId = req.body.pergunta;
+
+    Resposta.create({
+        corpo: corpo,
+        perguntaId: perguntaId
+    }).then(() => {
+        res.redirect("/pergunta/" + perguntaId);
     });
 });
 
