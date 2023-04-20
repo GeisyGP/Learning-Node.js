3837227e0e3604a433163baa8e7c29b17cb866bb
Geisy Poncio <geisygabri@gmail.com>
2023-04-20 11:59:02 -0300

    feat: add service to register appointments

--- projeto3-Agendamento/index.js
+++ projeto3-Agendamento/index.js
@@ -2,6 +2,7 @@ const express = require("express");
 const app = express();
 const bodyParser = require("body-parser");
 const mongoose = require("mongoose");
+const appointmentService = require("./services/AppointmentService");
 
 app.use(express.static("public"));
 app.use(bodyParser.urlencoded({extended: false}));
