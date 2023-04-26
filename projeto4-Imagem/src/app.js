6c6b1cfddc81f856f14ead85d788ff2071f6854a
Geisy Poncio <geisygabri@gmail.com>
2023-04-26 11:06:03 -0300

    feat: add login validation auth route with JWT

--- projeto4-Imagem/src/app.js
+++ projeto4-Imagem/src/app.js
@@ -3,6 +3,8 @@ let app = express();
 let mongoose = require("mongoose");
 let user = require("./models/user");
 let bcrypt = require("bcrypt");
+let jwt = require("jsonwebtoken");
+let JWTSecret = "ahauahsjajskaksa";
 
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
@@ -45,5 +47,38 @@ app.post("/user", async (req, res) => {
         res.sendStatus(500);
     }
 });
+
+app.post("/auth", async (req, res) => {
+    let {email, password} = req.body;
+
+    let user = await User.findOne({"email": email});
+    if (user == undefined){
+        res.statusCode = 403;
+        res.json({errors: {email: "E-mail não cadastrado"}});
+        return;
+    }
+
+    let isPasswordRight = await bcrypt.compare(password, user.password)
+    if(!isPasswordRight){
+        res.statusCode = 403;
+        res.json({errors: {password: "Senha incorreta"}});
+        return;
+    }
+
+    jwt.sign({email}, JWTSecret, {expiresIn: "48h"}, (err, token) => {
+        if(err){
+            res.sendStatus(500);
+            console.log(err);
+        }
+        else {
+            res.json({ token });
+        }
+    });
+});
+
+app.delete("/user/:email", async (req, res) => {
+    await User.deleteOne({"email": req.params.email});
+    res.sendStatus(200);
+})
 
 module.exports = app;
--- projeto4-Imagem/test/user.test.js
+++ projeto4-Imagem/test/user.test.js
@@ -2,6 +2,21 @@ let app = require("../src/app");
 let supertest = require("supertest");
 let request = supertest(app);
 
+let mainUser = {name: "Ana", email: "ana@email.com", password: "123456"};
+
+beforeAll(() => {
+    return request.post("/user")
+    .send(mainUser)
+    .then(res => {})
+    .catch(err => {console.log(err)});
+});
+
+afterAll(() => {
+    return request.delete(`/user/${mainUser.email}`)
+    .then(res => {})
+    .catch(err => {console.log(err)});
+});
+
 describe("Cadastro de usuário", () => {
     test("Deve cadastrar um usuário com sucesso", () => {
         let time = Date.now();
@@ -44,6 +59,41 @@ describe("Cadastro de usuário", () => {
             });
         }).catch(err => {
             fail(err)
+        });
+    });
+});
+
+describe("Autenticação", () => {
+    test("Deve retornar token quando logar", () => {
+        return request.post("/auth")
+        .send({email: mainUser.email, password: mainUser.password})
+        .then(res => {
+            expect(res.statusCode).toEqual(200);
+            expect(res.body.token).toBeDefined();
+        }).catch(err => {
+            fail(err);
+        });
+    });
+
+    test("Deve impedir que um usuário não cadastrado se logue", () => {
+        return request.post("/auth")
+        .send({email: "umemail@qualquer.com", password: "215896"})
+        .then(res => {
+            expect(res.statusCode).toEqual(403);
+            expect(res.body.errors.email).toEqual("E-mail não cadastrado");
+        }).catch(err => {
+            fail(err);
+        });
+    });
+
+    test("Deve impedir que um usuário se logue com senha errada", () => {
+        return request.post("/auth")
+        .send({email: mainUser.email, password: "nuvens"})
+        .then(res => {
+            expect(res.statusCode).toEqual(403);
+            expect(res.body.errors.password).toEqual("Senha incorreta");
+        }).catch(err => {
+            fail(err);
         });
     });
 });
